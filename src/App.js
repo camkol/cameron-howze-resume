6dd348e6f9f7252dffde8adcb5e76bde00315acc
camkol <koliyus3@hotmail.com>
2024-10-25 11:43:04 -0400

    set font and other stuff

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
+import Objective from "./components/Objective";
+import { resumeData } from "./resumeData";
 import "./App.css";
 
 function App() {
+  const skills = resumeData.skills;
+
+  console.log(skills);
+
   return (
     <div className="App">
       <h1>Cameron Howze</h1>
       <h2>Front-end Web Developer</h2>
+      <Objective />
       <Contact />
       <Skills />
       <Projects />
---
+++ src/components/Objective.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Objective() {
+  return (
+    <div className="objective">
+      <h3>Objective</h3>
+      <p>
+        Dedicated to creating user-centric, impactful software solutions that
+        enhance front-end and software development practices. Committed to
+        continuous learning and collaboration with professionals to innovate and
+        push technological boundaries for the advancement of the industry.
+      </p>
+    </div>
+  );
+}
+
+export default Objective;
