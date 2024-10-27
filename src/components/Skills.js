de11d2bc3315e77c6ac69f442d7710bd0c406550
camkol <koliyus3@hotmail.com>
2024-10-27 18:34:08 -0400

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,15 +7,6 @@ function Skills({ skills }) {
       {Object.entries(skills).map(([category, details]) => (
         <Category key={category} name={category} details={details} />
       ))}
-      <ul>
-        <li>Programming Languages: JavaScript, PHP</li>
-        <li>Front-end: HTML, CSS, Bootstrap, SASS, ReactJS, Redux, jQuery</li>
-        <li>Back-end: Node.js, Express.js</li>
-        <li>Version Control: Git, GitHub</li>
-        <li>Testing: Mocha, Jest</li>
-        <li>Problem Solving: Analytical Thinking, Troubleshooting</li>
-        <li>Collaboration: Team Player, Effective Communicator</li>
-      </ul>
     </div>
   );
 }
@@ -26,16 +17,8 @@ function Category({ name, details }) {
   return (
     <div className="category">
       <p style={{ textDecoration: "underline" }}>{name}:</p>
-      <p>
-        {details
-          .map((skill, index) => (
-            <span key={index}>
-              {skill}
-              {index < details.length - 1 ? ", " : ""}
-            </span>
-          ))
-          .join(",")}
-      </p>
+      <p>{details.join(", ")}</p>
+      {/* Converts the array into a comma-separated string */}
     </div>
   );
 }
