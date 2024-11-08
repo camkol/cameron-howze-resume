4005fcac2010c032b02a23f75114a318565d14ab
camkol <koliyus3@hotmail.com>
2024-11-08 07:44:05 -0500

    Update index.js

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Education.css";
 import Tooltip from "../Tooltip";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 function Education({ education }) {
